a73cc81599f63f068d3f4d4e379f4264aac26838
hdf1986 <hugo.farji+github@gmail.com>
2020-01-18 16:08:08 -0300

    Added express validations

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+// If we had some orm, we can probably
+// use that as validator instead of express-validator
+// This seems cleaner to me than adding conditionals to the create function
+const { body } = require('express-validator');
 const app = express();
 const port = process.env.PORT || 3000;
 const { transactionsIndex, transactionsShow, transactionsCreate } = require('./src/controllers/transactions');
@@ -10,7 +14,10 @@ const transactionsList = []
 app.use(bodyParser.json())
 
 app.get('/api/v1/transactions', transactionsIndex(transactionsList));
-app.post('/api/v1/transactions', transactionsCreate(transactionsList));
+app.post('/api/v1/transactions', [
+  body('type').notEmpty().isIn(['debit', 'credit']),
+  body('amount').notEmpty().isFloat(),
+], transactionsCreate(transactionsList));
 app.get('/api/v1/transactions/:id', transactionsShow(transactionsList));
 
 app.get('/api/v1/balance', balanceCalculate(transactionsList));
--- src/controllers/transactions.js
+++ src/controllers/transactions.js
@@ -1,19 +1,28 @@
 const uuid = require('uuid-random');
+// If we had some orm, we can probably
+// use that as validator instead of express-validator
+// This seems cleaner to me than adding conditionals to the create function
+const { validationResult } = require('express-validator');
 
 const transactionsIndex = (transactionsList) => (req, res) => {
   res.send(transactionsList);
 };
 
 const transactionsCreate = (transactionsList) => (req, res) => {
-  const { type, amount } = req.body
-  const newTransaction = {
-    id: uuid(),
-    type,
-    amount,
-    effectiveDate: new Date()
-  };
-  transactionsList.push(newTransaction);
-  res.status(201).send(newTransaction);
+  const errors = validationResult(req)
+  if(errors.isEmpty()) {
+    const { type, amount } = req.body
+    const newTransaction = {
+      id: uuid(),
+      type,
+      amount: parseFloat(amount),
+      effectiveDate: new Date()
+    };
+    transactionsList.push(newTransaction);
+    res.status(201).send(newTransaction);
+  } else {
+    res.status(422).send(errors)
+  }
 }
 
 const transactionsShow = (transactionsList) => (req, res) => {
